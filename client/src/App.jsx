386c8807ad585e9ccc893543f4c26410dc3cfaae
swar03 <swar.c@somaiya.edu>
2025-08-18 09:38:25 +0530

    corrected padding layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,24 @@ import Training from "./components/Training";
 import TrainingInternship from "./components/TrainingInternship";
 import LoginRegister from "./components/LoginRegister";
 import Teach from "./components/Teach";
+
+// Make sure this path points to your main CSS file (e.g., App.css or index.css)
+import './App.css'; 
+
 export default function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-         <Route path="/teach" element={<Teach />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/training" element={<Training />} />
-        <Route path="/training-internship" element={<TrainingInternship />} />
-        <Route path="/login" element={<LoginRegister />} />
-      </Routes>
+
+      <main>
+        <Routes>
+          <Route path="/teach" element={<Teach />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/training" element={<Training />} />
+          <Route path="/training-internship" element={<TrainingInternship />} />
+          <Route path="/login" element={<LoginRegister />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
